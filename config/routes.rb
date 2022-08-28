13ab835d345c5a7382545901c7705644d4bf91ee
EC2 Default User <ec2-user@ip-172-31-9-38.us-west-1.compute.internal>
2022-08-28 08:18:25 +0000

    [add]public 新規登録機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
  
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about',as:'about'
+  end
+  
   namespace :admin do
     resources :customers,only:[:new, :create, :index, :show ,:edit,:update]
   end
