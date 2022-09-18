56621491e4d69db2bc9c2670f4287ff46625dd3c
EC2 Default User <ec2-user@ip-172-31-9-38.us-west-1.compute.internal>
2022-09-18 08:00:22 +0000

    [add]注文ステータスと制作ステータスの連動

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,12 +3,15 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail=OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
-    if @order_details.making_status=="manufacturing"
-      order.update(order_status:"paid_up")
-    end  
+    if @order_detail.making_status=="manufacturing" #注文はひとつだけだからeach文はいらない
+    @order_detail.order.update(order_status: "paid_up")  #order.updateだけだとorderの定義がないから@order_detailas.を書きアソシエーションにすることでorderの定義ができる
+    end
+    if@order_detail.making_status=="finish"
+    @order_detail.order.update(order_status:"preparing")  
+    end
     redirect_to admin_order_path(@order_detail.order.id)#こっちもorderページに行きたいから（@order_detail）ではダメ アソシエーションの記述をする だけど(order.id)だけだとorderの定義をこっちのページにしてないから@order_detailをつける
-  end  #(@order_detail.order.id)こういう記述はmodelにアソシエーションの記述した時だけ使える　(@order_detail.order.id)これだけではアソシエーションしたことにならない
- 
+    #(@order_detail.order.id)こういう記述はmodelにアソシエーションの記述した時だけ使える(@order_detail.order.id)これだけではアソシエーションしたことにならない
+  end
   def order_detail_params
      params.require(:order_detail).permit(:making_status)
   end    
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,9 +13,9 @@ class Admin::OrdersController < ApplicationController
   def update
     @order=Order.find(params[:id])
     @order.update(order_params)
-    if @order.order_status == "confirmation"#連動の書き方は最初にもしこうならこうするという結果を最初にかくとわかりやすい　(16と１９）
+  if @order.order_status == "confirmation"#連動の書き方は最初にもしこうならこうするという結果を最初にかくとわかりやすい (16と１９）
       @order_details=@order.order_details #したの＠order_detailsだけだと定義がないからだめ
-      @order_details.each do |order_detail| #何に対してupdateするのか書かないといけない(order_detailsに対して)　一回のupdateでは一つしかupdateできない order_details は複数だからeach文で一つずつupdateする
+      @order_details.each do |order_detail| #何に対してupdateするのか書かないといけない(order_detailsに対して) 一回のupdateでは一つしかupdateできない order_details は複数だから(なぜなら一つの注文にいっぱいあるから)each文で一つずつupdateする
       order_detail.update(making_status: "waiting_manufacture")
     end 
   end
