5c29c2c11ea5edf064b90314014abec046b9c4a2
EC2 Default User <ec2-user@ip-172-31-9-38.us-west-1.compute.internal>
2022-09-21 12:21:03 +0000

    [finish]nagano_cake

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,18 +1,23 @@
 class Admin::OrderDetailsController < ApplicationController
-  
-  def update
-    @order_detail=OrderDetail.find(params[:id])
-    @order_detail.update(order_detail_params)
-    if @order_detail.making_status=="manufacturing" #注文はひとつだけだからeach文はいらない
-    @order_detail.order.update(order_status: "paid_up")  #order.updateだけだとorderの定義がないから@order_detailas.を書きアソシエーションにすることでorderの定義ができる
+    def update
+      @order_detail=OrderDetail.find(params[:id])
+      @order=@order_detail.order
+      @order_detail.update(order_detail_params)
+      if @order_detail.making_status=="manufacturing" #注文はひとつだけだからeach文はいらない
+         @order.update(order_status: "paid_up")  #order.updateだけだとorderの定義がないから@order_detailas.を書きアソシエーションにすることでorderの定義ができる
+      end
+      @order_details=@order.order_details#9行目の後ろのorder_details定義してないから使えないじゃんと思うがアソシエーション（has manyとか）だから定義してなくていい９行目の最初の@order_detailは変数だから絶対定義しないとエラーになる　
+      puts @order_details.nil?
+      #@order_details = ["impossible_manufacture", "waiting_manufacture", "manufacturing", "finish"]
+      if @order_details.all? { |w| w.making_status =="finish" }#wは@order_detaisを一つずつ取り出したやつ
+        @order.update(order_status:"preparing")  
+      end
+      
+      redirect_to admin_order_path(@order.id)#こっちもorderページに行きたいから（@order_detail）ではダメ アソシエーションの記述をする だけど(order.id)だけだとorderの定義をこっちのページにしてないから@order_detailをつける
+      #(@order_detail.order.id)こういう記述はmodelにアソシエーションの記述した時だけ使える(@order_detail.order.id)これだけではアソシエーションしたことにならない
     end
-    if@order_detail.making_status=="finish"
-    @order_detail.order.update(order_status:"preparing")  
-    end
-    redirect_to admin_order_path(@order_detail.order.id)#こっちもorderページに行きたいから（@order_detail）ではダメ アソシエーションの記述をする だけど(order.id)だけだとorderの定義をこっちのページにしてないから@order_detailをつける
-    #(@order_detail.order.id)こういう記述はmodelにアソシエーションの記述した時だけ使える(@order_detail.order.id)これだけではアソシエーションしたことにならない
-  end
-  def order_detail_params
-     params.require(:order_detail).permit(:making_status)
-  end    
+def order_detail_params
+ params.require(:order_detail).permit(:making_status)
+end    
 end
+ 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,12 +11,14 @@ class Admin::OrdersController < ApplicationController
   def update
     @order=Order.find(params[:id])
     @order.update(order_params)
-  if @order.order_status == "confirmation"#連動の書き方は最初にもしこうならこうするという結果を最初にかくとわかりやすい (16と１９）
-      @order_details=@order.order_details #したの＠order_detailsだけだと定義がないからだめ
+    if @order.order_status == "confirmation"#連動の書き方は最初にもしこうならこうするという結果を最初にかくとわかりやすい (16と１９）
+      @order_details=@order.order_details#したの＠order_detailsだけだと定義がないからだめ
+      
       @order_details.each do |order_detail| #何に対してupdateするのか書かないといけない(order_detailsに対して) 一回のupdateでは一つしかupdateできない order_details は複数だから(なぜなら一つの注文にいっぱいあるから)each文で一つずつupdateする
-      order_detail.update(making_status: "waiting_manufacture")
+        @order_details.update(making_status: "waiting_manufacture")
+      end
+      
     end 
-  end
     redirect_to admin_order_path(@order)
   end
   
